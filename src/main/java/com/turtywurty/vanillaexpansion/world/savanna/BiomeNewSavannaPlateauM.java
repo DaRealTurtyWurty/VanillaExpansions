632885eea715b63bedef543073959f40e005c2c6
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2019-06-06 15:26:21 +0100

    Savanna Biomes

---
+++ src/main/java/com/turtywurty/vanillaexpansion/world/savanna/BiomeNewSavannaPlateauM.java
@@ -0,0 +1,45 @@
+package com.turtywurty.vanillaexpansion.world.savanna;
+
+import java.util.Random;
+
+import net.minecraft.block.BlockDirt;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeSavannaMutated;
+import net.minecraft.world.chunk.ChunkPrimer;
+
+public class BiomeNewSavannaPlateauM extends BiomeNewSavanna
+{
+	public BiomeNewSavannaPlateauM()
+    {
+        super(new BiomeProperties("NewSavannaPlateauM").setBaseBiome("savanna_rock").setBaseHeight(1.05F).setHeightVariation(1.2125001F).setTemperature(1.0F).setRainfall(0.0F).setRainDisabled());
+        this.decorator.treesPerChunk = 2;
+        this.decorator.flowersPerChunk = 2;
+        this.decorator.grassPerChunk = 5;
+    }
+
+    public void genTerrainBlocks(World worldIn, Random rand, ChunkPrimer chunkPrimerIn, int x, int z, double noiseVal)
+    {
+        this.topBlock = Blocks.GRASS.getDefaultState();
+        this.fillerBlock = Blocks.DIRT.getDefaultState();
+
+        if (noiseVal > 1.75D)
+        {
+            this.topBlock = Blocks.STONE.getDefaultState();
+            this.fillerBlock = Blocks.STONE.getDefaultState();
+        }
+        else if (noiseVal > -0.5D)
+        {
+            this.topBlock = Blocks.DIRT.getDefaultState().withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.COARSE_DIRT);
+        }
+
+        this.generateBiomeTerrain(worldIn, rand, chunkPrimerIn, x, z, noiseVal);
+    }
+
+    public void decorate(World worldIn, Random rand, BlockPos pos)
+    {
+        this.decorator.decorate(worldIn, rand, this, pos);
+    }
+}
