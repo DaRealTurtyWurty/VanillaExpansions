0acc9ee855af24a515204ae497c16f930541a5a6
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2019-06-11 19:46:42 +0100

    Added Cactus Juice

--- src/main/java/com/turtywurty/vanillaexpansion/handlers/EventHandler.java
+++ src/main/java/com/turtywurty/vanillaexpansion/handlers/EventHandler.java
@@ -1,5 +1,6 @@
 package com.turtywurty.vanillaexpansion.handlers;
 
+import com.turtywurty.vanillaexpansion.events.CactusJuiceEvent;
 import com.turtywurty.vanillaexpansion.events.LootTableEvent;
 
 import net.minecraftforge.common.MinecraftForge;
@@ -9,7 +10,9 @@ public class EventHandler
 	public static void registerEvents()
 	{
 		LootTableEvent lootEvent = new LootTableEvent();
+		CactusJuiceEvent cactusEvent = new CactusJuiceEvent();
 		
 		MinecraftForge.EVENT_BUS.register(lootEvent);
+		MinecraftForge.EVENT_BUS.register(cactusEvent);
 	}
 }
